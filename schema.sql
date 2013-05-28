b84507b0e0aedccb2f0e2e8f6627016eb55da1ab
Martin Keegan <martin@no.ucant.org>
2013-05-28 11:45:15 +0100

    update schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,5 @@
 
-create variable (
-	   var_id serial primary key, 
-	   name varchar(30) not null, 
-	   description varchar(100) not null
-);
-
-create table variable (
+create table var (
 	   var_id serial primary key,
 	   name varchar(30) not null,
 	   description varchar(100) not null
@@ -13,7 +7,7 @@ create table variable (
 
 create table result_set (
 	   rs_id serial primary key,
-	   dependent_var int not null references variable (var_id)
+	   dependent_var int not null references var(var_id)
 );
 
 create table model (model_id serial primary key,
@@ -25,7 +19,7 @@ create table model (model_id serial primary key,
 
 create table effect (
 	   model_id integer not null references model(model_id),
-	   var_id integer not null references variable(var_id),	
+	   var_id integer not null references var(var_id),	
 	   coefficient real not null,
 	   standard_error real not null
 );
