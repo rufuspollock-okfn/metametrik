42c23988afcefd92834bb4fca9b5051828698aba
Martin Keegan <martin@no.ucant.org>
2013-05-28 11:36:19 +0100

    add schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+
+create variable (
+	   var_id serial primary key, 
+	   name varchar(30) not null, 
+	   description varchar(100) not null
+);
+
+create table variable (
+	   var_id serial primary key,
+	   name varchar(30) not null,
+	   description varchar(100) not null
+);
+
+create table result_set (
+	   rs_id serial primary key,
+	   dependent_var int not null references variable (var_id)
+);
+
+create table model (model_id serial primary key,
+	   	name varchar(30) not null,
+		description varchar(100) not null,
+		r_squared real,
+		sample_size integer
+);
+
+create table effect (
+	   model_id integer not null references model(model_id),
+	   var_id integer not null references variable(var_id),	
+	   coefficient real not null,
+	   standard_error real not null
+);
